33281830b3d6625edd319460df51202e6e8c695e
Ty Jenkins <jenkins.ty@gmail.com>
2016-02-25 15:53:51 +0000

    Altered to submit from a form mapping to a domain object

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,26 @@
+package hello;
+
+/**
+ * Created by Jenkins on 25/02/2016.
+ */
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,8 +2,9 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Jenkins on 25/02/2016.
@@ -11,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
         return "greeting";
+    }
+
+    @RequestMapping(value="/greeting", method= RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
     }
 }
