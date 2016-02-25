a3f83659d62af5bee5d62b6ee3996d02e873b18d
Ty Jenkins <jenkins.ty@gmail.com>
2016-02-25 18:15:22 +0000

    Added H2 in memory DB maven config and set up local host console

---
+++ src/main/java/hello/WebConfiguration.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Jenkins on 25/02/2016.
+ */
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
